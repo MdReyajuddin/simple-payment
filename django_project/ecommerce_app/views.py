d1b166088cca832dfea369c7c6d3a2658eb9814c
MdReyajuddin <md.reyajuddin45@gmail.com>
2020-04-28 22:41:40 +0530

    first commit

---
+++ django_project/ecommerce_app/views.py
@@ -0,0 +1,146 @@
+from django.shortcuts import render, HttpResponse, redirect, \
+    get_object_or_404, reverse
+from django.contrib import messages
+from .models import Product, Order, LineItem
+from .forms import CartForm, CheckoutForm
+from . import cart
+from paypal.standard.forms import PayPalPaymentsForm
+from django.views.decorators.csrf import csrf_exempt
+from .paytm import Checksum
+from decimal import *
+
+MERCHANT_KEY= 'QBto&17QqF1Em1gQ'
+# Create your views here.
+
+
+def index(request):
+    all_products = Product.objects.all()
+    return render(request, "ecommerce_app/index.html", {
+                                    'all_products': all_products,
+                                    })
+
+
+def show_product(request, product_id, product_slug):
+    product = get_object_or_404(Product, id=product_id)
+
+    if request.method == 'POST':
+        form = CartForm(request, request.POST)
+        if form.is_valid():
+            request.form_data = form.cleaned_data
+            cart.add_item_to_cart(request)
+            return redirect('show_cart')
+
+    form = CartForm(request, initial={'product_id': product.id})
+    return render(request, 'ecommerce_app/product_detail.html', {
+                                            'product': product,
+                                            'form': form,
+                                            })
+
+
+def show_cart(request):
+
+    if request.method == 'POST':
+        if request.POST.get('submit') == 'Update':
+            cart.update_item(request)
+        if request.POST.get('submit') == 'Remove':
+            cart.remove_item(request)
+
+    cart_items = cart.get_all_cart_items(request)
+    cart_subtotal = cart.subtotal(request)
+    return render(request, 'ecommerce_app/cart.html', {
+                                            'cart_items': cart_items,
+                                            'cart_subtotal': cart_subtotal,
+                                            })
+
+
+def checkout(request):
+    if request.method == 'POST':
+        form = CheckoutForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            o = Order(
+                name = cleaned_data.get('name'),
+                email = cleaned_data.get('email'),
+                postal_code = cleaned_data.get('postal_code'),
+                address = cleaned_data.get('address'),
+            )
+            o.save()
+
+            all_items = cart.get_all_cart_items(request)
+            for cart_item in all_items:
+                li = LineItem(
+                    product_id=cart_item.product_id,
+                    price = cart_item.price,
+                    quantity = cart_item.quantity,
+                    order_id = o.id
+                )
+
+                li.save()
+
+            cart.clear(request)
+            request.session['order_id'] = o.id
+            return redirect('process_payment')
+    else:
+        form = CheckoutForm()
+        return render(request, 'ecommerce_app/checkout.html', {'form': form})
+
+
+def process_payment(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+    host = request.get_host()
+
+    # paypal_dict = {
+    #     # 'business': settings.PAYPAL_RECEIVER_EMAIL,
+    #     # 'amount': '%.2f' % order.total_cost().quantize(
+    #         # Decimal('.01')),
+    #     'item_name': 'Order {}'.format(order.id),
+    #     'invoice': str(order.id),
+    #     'currency_code': 'USD',
+    #     'notify_url': 'http://{}{}'.format(host,
+    #                                        reverse('paypal-ipn')),
+    #     'return_url': 'http://{}{}'.format(host,
+    #                                        reverse('payment_done')),
+    #     'cancel_return': 'http://{}{}'.format(host,
+    #                                           reverse('payment_cancelled')),
+    # }
+    #
+    # form = PayPalPaymentsForm()
+    # return render(request, 'ecommerce_app/process_payment.html', {'order': order, 'form': form})
+    param_dict = {
+
+        'MID': 'fAwVcZ17670747402537',
+        'ORDER_ID': str(order.id),
+        'TXN_AMOUNT': '%.2f' % order.total_cost().quantize(Decimal('.01')),
+        'CUST_ID': 'reyajurs@gmail.com',
+        'INDUSTRY_TYPE_ID': 'Retail',
+        'WEBSITE': 'WEBSTAGING',
+        'CHANNEL_ID': 'WEB',
+        'CALLBACK_URL': 'http://127.0.0.1:8000/payment_done/',
+
+    }
+
+    param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
+    return render(request, 'blog/process_payment.html', {'param_dict': param_dict})
+
+@csrf_exempt
+def payment_done(request):
+    checksum=0
+    form = request.POST
+    response_dict = {}
+    for i in form.keys():
+        response_dict[i] = form[i]
+        if i == 'CHECKSUMHASH':
+            checksum = form[i]
+
+    verify = Checksum.verify_checksum(response_dict, MERCHANT_KEY, checksum)
+    if verify:
+        if response_dict['RESPCODE'] == '01':
+            print('order successful')
+        else:
+            print('order was not successful because' + response_dict['RESPMSG'])
+    return render(request, 'blog/paymentstatus.html', {'response': response_dict})
+
+# @csrf_exempt
+# def payment_canceled(request):
+#     return render(request, 'ecommerce_app/payment_cancelled.html')
